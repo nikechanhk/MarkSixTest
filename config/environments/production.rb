698e8b15f9885f3d61c4dfc35dd4f85147de2527
Nike Chan <nike.chan@101medialab.com>
2020-06-14 23:33:29 +0800

    fix config hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "prod.eba-mp3jqm5m.ap-southeast-1.elasticbeanstalk.com"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,7 +33,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://prod.eba-mp3jqm5m.ap-southeast-1.elasticbeanstalk.com/'
+  # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
